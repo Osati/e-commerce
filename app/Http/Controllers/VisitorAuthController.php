7fb3d411165bc57cde5c44a4946ab77ad70087db
Anis <aniscs452@gmail.com>
2023-10-18 21:21:04 +0600

    user add form create

--- app/Http/Controllers/VisitorAuthController.php
+++ app/Http/Controllers/VisitorAuthController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Visitor;
 use Illuminate\Http\Request;
 
 class VisitorAuthController extends Controller
 {
     public function signupView(){
         return view('front-end.auth.signup');
+    }
+    public function signStore($request){
+        Visitor::saveinfo($request);
+        return redirect(route('home'));
     }
 }
--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Visitor extends Model
 {
     use HasFactory;
+    public static $visitor;
+
+    public static function saveinfo($request){
+        self::$visitor = new Visitor();
+        self::$visitor->$request->username;
+        self::$visitor->email = $request->email;
+        self::$visitor->phone = $request->phone;
+        self::$visitor->password = $request->password;
+        self::$visitor->save();
+    }
 }
